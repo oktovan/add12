72e974ee799a7703de8c9dfde4d63b51d2538121
oktovan <oktovan@users.noreply.github.com>
2020-07-21 08:45:17 +0700

    Create add12.sh

---
+++ add12.sh
@@ -0,0 +1,5 @@
+git clone -b 12.0 https://github.com/oca/server-ux.git
+git clone -b 12.0 https://github.com/oca/reporting-engine.git
+git clone -b 12.0 https://github.com/oca/server-tools.git
+git clone -b 12.0 https://github.com/oca/stock-logistics-workflow
+git clone https://github.com/oktovan/prune12
