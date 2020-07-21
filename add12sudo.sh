3336b2f4b6fc5fd0a4e1341f67735558215c3c7f
oktovan <oktovan@users.noreply.github.com>
2020-07-21 08:46:04 +0700

    Create add12sudo.sh

---
+++ add12sudo.sh
@@ -0,0 +1,5 @@
+sudo git clone -b 12.0 https://github.com/oca/server-ux.git
+sudo git clone -b 12.0 https://github.com/oca/reporting-engine.git
+sudo git clone -b 12.0 https://github.com/oca/server-tools.git
+sudo git clone -b 12.0 https://github.com/oca/stock-logistics-workflow
+sudo git clone https://github.com/oktovan/prune12
