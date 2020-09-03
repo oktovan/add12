a1f6d05b81794c24a34892fc8c8e72f46f5100ad
oktovan <oktovan@users.noreply.github.com>
2020-09-03 16:17:55 +0700

    Update add12.sh

--- add12.sh
+++ add12.sh
@@ -1,5 +1,51 @@
-git clone -b 12.0 https://github.com/oca/server-ux.git
-git clone -b 12.0 https://github.com/oca/reporting-engine.git
-git clone -b 12.0 https://github.com/oca/server-tools.git
-git clone -b 12.0 https://github.com/oca/stock-logistics-workflow
 git clone https://github.com/oktovan/prune12
+
+git clone -b 12.0 https://github.com/oca/account-analytic
+git clone -b 12.0 https://github.com/OCA/account-closing
+git clone -b 12.0 https://github.com/oca/account-invoicing
+git clone -b 12.0 https://github.com/oca/account-financial-reporting
+git clone -b 12.0 https://github.com/oca/account-financial-tools 
+git clone -b 12.0 https://github.com/oca/contract
+git clone -b 12.0 https://github.com/oca/intrastat
+git clone -b 12.0 https://github.com/oca/mis-builder
+git clone -b 12.0 https://github.com/oca/manufacture
+git clone -b 12.0 https://github.com/oca/operating-unit
+git clone -b 12.0 https://github.com/oca/partner-contact
+git clone -b 12.0 https://github.com/OCA/purchase-workflow
+git clone -b 12.0 https://github.com/oca/product-attribute
+git clone -b 12.0 https://github.com/oca/reporting-engine
+git clone -b 12.0 https://github.com/OCA/sale-workflow
+git clone -b 12.0 https://github.com/oca/server-tools
+git clone -b 12.0 https://github.com/oca/server-ux.git
+git clone -b 12.0 https://github.com/oca/stock-logistics-transport
+git clone -b 12.0 https://github.com/oca/stock-logistics-warehouse
+git clone -b 12.0 https://github.com/oca/stock-logistics-workflow
+git clone -b 12.0 https://github.com/oca/web
+git clone -b 12.0 https://github.com/OCA/product-variant
+git clone -b 12.0 https://github.com/OCA/website
+
+git clone -b 12.0 https://github.com/odoomrp/odoomrp-utils.git
+git clone -b 12.0 https://github.com/odoomrp/odoomrp-wip
+
+git clone -b 12.0 https://github.com/OdooCommunityWidgets/website_multi_image.git
+
+# sudo git clone -b 8.0 https://github.com/open-synergy/opnsynid-account-closing
+# sudo git clone -b 8.0 https://github.com/open-synergy/opnsynid-account-invoicing
+# sudo git clone -b 8.0 https://github.com/open-synergy/opnsynid-accounting-report
+# sudo git clone -b 8.0 https://github.com/open-synergy/opnsynid-edii
+# sudo git clone -b 8.0 https://github.com/open-synergy/opnsynid-hr
+# sudo git clone -b 8.0 https://github.com/open-synergy/opnsynid-l10n-indonesia
+# sudo git clone -b 8.0 https://github.com/open-synergy/opnsynid-manufacture
+# sudo git clone -b 8.0 https://github.com/open-synergy/opnsynid-odoo-curated
+# sudo git clone -b 8.0 https://github.com/open-synergy/opnsynid-operating-unit
+# sudo git clone -b 8.0 https://github.com/open-synergy/opnsynid-partner-contact
+# sudo git clone -b 8.0 https://github.com/open-synergy/opnsynid-purchase-workflow
+# sudo git clone -b 8.0 https://github.com/open-synergy/opnsynid-product-attribute
+# sudo git clone -b 8.0 https://github.com/open-synergy/opnsynid-sale-workflow
+# sudo git clone -b 8.0 https://github.com/open-synergy/opnsynid-server-tools
+# sudo git clone -b 8.0 https://github.com/open-synergy/opnsynid-stock-logistics-warehouse
+# sudo git clone -b 8.0 https://github.com/open-synergy/opnsynid-stock-logistics-workflow
+# sudo git clone -b 8.0 https://github.com/open-synergy/opnsynid-stock-reporting
+# sudo git clone https://github.com/oktovan/invent2.git
+# sudo git clone -b 8.0 https://github.com/open-synergy/opnsynid-l10n-indonesia-report
+# sudo git clone -b 8.0 https://github.com/open-synergy/opnsynid-sale-reporting
